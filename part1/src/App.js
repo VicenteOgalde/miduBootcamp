a453dc846e7cd8327f321c8b273f534078762b9b
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-23 18:21:55 -0300

    commit 1 react

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Message from './Message';
 
-function App() {
+const Description=()=>{
+  return  <strong>Working</strong>
+}
+
+const App=()=> {
+  
   return (
     <div className="App">
-      Hello World
+      <Message color="red" msg='React'/>
+      <Message color="green" msg='BootCamp'/>
+      <Description />     
+      
     </div>
   );
 }
---
+++ part1/src/Message.js
@@ -0,0 +1,5 @@
+const Message =(props)=>{
+    return(<> <h1 style={{color:props.color}}>{props.msg}</h1></>);
+}
+
+export default Message;
